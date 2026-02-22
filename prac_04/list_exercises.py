a943a56f544c49e4f78b0bf06dd5f2212e6cd9bc
ABCXXX <jinhui.zhao@my.jcu.edu.au>
2026-02-22 23:20:50 +0800

    codes give user information of a set of number user have input

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,12 @@
+
+user_numbers = []
+
+for i in range(5):
+
+    number = int(input("Enter a number: "))
+    user_numbers.append(number)
+print(f"The first number is {user_numbers[0]}")
+print(f"The last number is {user_numbers[-1]}")
+print(f"The smallest number is {min(user_numbers)}")
+print(f"The largest number is {max(user_numbers)}")
+print(f"The average number is {sum(user_numbers)/len(user_numbers)}")
