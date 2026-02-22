ecde387f6252b413087cc792b12418d9ab738c7c
ABCXXX <jinhui.zhao@my.jcu.edu.au>
2026-02-22 22:51:18 +0800

    warm me up

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+ANSWER:
+3
+2
+1
+[3, 1, 4, 1, 5, 9, 2]
+[1, 5]
+True
+False
+False
+[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
+
+numbers[0]  = "Ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
