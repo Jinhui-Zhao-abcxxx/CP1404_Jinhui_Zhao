4139780246e65b69c44448c42ab0b33e5939377f
ABCXXX <jinhui.zhao@my.jcu.edu.au>
2026-03-08 19:05:48 +0800

    complete display part with sorted data

--- assignment/assignment 1/assignment 1.py
+++ assignment/assignment 1/assignment 1.py
@@ -4,6 +4,7 @@ Jinhui.Zhao
 """
 import random
 from operator import itemgetter
+
 MENU = "Menu: \nD - Display all places \nR - Recommend a random place \nA - Add a new place \nM - Mark a place as visited \nQ - Quit "
 
 
@@ -12,17 +13,40 @@ def main():
     print("Travel Tracker 1.0 - by Jinhui.Zhao")
     original_places = read_csv()
     # print(original_places)
-    # """this is used for testing"""
+    # this is used for testing
     visited_places,unvisited_places = clean_data(original_places)
     print(visited_places)
     print(unvisited_places)
-    # """this is used for testing"""
+    # this is used for testing
     print(f"{len(original_places)} places loaded from places.csv")
     show_menu()
     user_input = input(">>>".lower())
     while user_input != "q":
         if user_input == "d":
-            print()
+
+            max_length_name = max(get_max_length(0,visited_places),get_max_length(0,unvisited_places))
+            max_length_country = max(get_max_length(1,visited_places),get_max_length(1,unvisited_places))
+
+            print(max_length_name)
+            print(max_length_country)
+            # this is used for testing
+
+            display_number = 0
+
+            display_places(display_number, max_length_country, max_length_name,unvisited_places,visited_places)
+
+            user_input = input(">>>".lower())
+
+
+def display_places(display_number, max_length_country, max_length_name,unvisited_places,visited_places,):
+    for place in unvisited_places:
+        display_number += 1
+        print(f"*{display_number}. {place[0]:<{max_length_name}} in {place[1]:<{max_length_country}} {place[2]}")
+
+    for place in visited_places:
+        display_number += 1
+        print(f"{display_number}. {place[0]:<{max_length_name}} in {place[1]:<{max_length_country}} {place[2]}")
+
 
 
 def clean_data(original_places):
@@ -33,6 +57,9 @@ def clean_data(original_places):
             unvisited_places.append(place_info[:-1].split(","))
         else:
             visited_places.append(place_info[:-1].split(","))
+
+    unvisited_places.sort(key = itemgetter(2))
+    unvisited_places.reverse()
 
     return visited_places,unvisited_places
 
@@ -45,6 +72,14 @@ def read_csv():
 
 def show_menu():
     print(MENU)
+
+def get_max_length(position,places_list):
+    measuring_list = []
+    for place in places_list:
+        measuring_list.append(place[position])
+    return len(max(measuring_list))
+
+
 
 
 
