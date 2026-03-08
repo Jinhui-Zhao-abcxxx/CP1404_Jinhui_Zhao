7ed63ab40a1f253149de5f4ca21d4e7933e13f49
ABCXXX <jinhui.zhao@my.jcu.edu.au>
2026-03-08 18:34:04 +0800

    clean the data to output cleaned data into visited_places and unvisited_places

--- assignment/assignment 1/assignment 1.py
+++ assignment/assignment 1/assignment 1.py
@@ -3,6 +3,7 @@ CP1404/CP5632 Assignment 1 – Travel Tracker 1.0
 Jinhui.Zhao
 """
 import random
+from operator import itemgetter
 MENU = "Menu: \nD - Display all places \nR - Recommend a random place \nA - Add a new place \nM - Mark a place as visited \nQ - Quit "
 
 
@@ -10,18 +11,30 @@ MENU = "Menu: \nD - Display all places \nR - Recommend a random place \nA - Add 
 def main():
     print("Travel Tracker 1.0 - by Jinhui.Zhao")
     original_places = read_csv()
-    # print(original_places)    """this is used for testing"""
-    cleaned_places = clean_data(original_places)
-    # print(cleaned_places)    """this is used for testing"""
-    print(f"{len(cleaned_places)} places loaded from places.csv")
+    # print(original_places)
+    # """this is used for testing"""
+    visited_places,unvisited_places = clean_data(original_places)
+    print(visited_places)
+    print(unvisited_places)
+    # """this is used for testing"""
+    print(f"{len(original_places)} places loaded from places.csv")
     show_menu()
+    user_input = input(">>>".lower())
+    while user_input != "q":
+        if user_input == "d":
+            print()
 
 
 def clean_data(original_places):
-    cleaned_places = []
+    visited_places = []
+    unvisited_places = []
     for place_info in original_places:
-        cleaned_places.append(place_info[:-3].split(","))
-    return cleaned_places
+        if place_info[-2] == "n":
+            unvisited_places.append(place_info[:-1].split(","))
+        else:
+            visited_places.append(place_info[:-1].split(","))
+
+    return visited_places,unvisited_places
 
 def read_csv():
     places_file = open("places.csv", "r")
